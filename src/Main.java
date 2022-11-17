fb645f082496fe5bb8cb5450b552a5bb9068b94d
Adanato <66979547+Adanato@users.noreply.github.com>
2022-11-17 11:39:26 -0500

    Setup some basic classes and methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.sql.SQLOutput;
+import java.util.*;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Game start
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String playerName = scan.nextLine();
+        Player player = new Player(playerName);
+        System.out.println("Hello " + player.getPlayerName());
+
+        //game loop
+        while(true){
+            System.out.println("What do you want to do?");
+            System.out.println("Here is a list of options:");
+            System.out.println("[1] Train your pet, [2] Battle others, [3]get more Rabbits, or [4] quit the game");
+            int action = Integer.valueOf(scan.nextInt());
+            if(action == 4)
+            {
+                System.out.println("Aight see you later!");
+                break;
+            }
+            System.out.println("ok");
+        }
+        System.out.println("Thanks for playing");
     }
+
 }
--- src/Player.java
+++ src/Player.java
@@ -8,4 +8,28 @@ public class Player {
         money = 0;
         pet = new Rabbit("Rabbo");
     }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public Rabbit getPet() {
+        return pet;
+    }
+
+    public void setPet(Rabbit pet) {
+        this.pet = pet;
+    }
 }
